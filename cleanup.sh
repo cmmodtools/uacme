ec8a87091a01bc876a9b674d3d7de2ed37150a5d
cmmodtools <40057577+cmmodtools@users.noreply.github.com>
2025-02-01 16:33:31 +1100

    Add certbot config

--- cleanup.sh
+++ cleanup.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-exec "${0%/*}/nsupdate.sh" done "$CERTBOT_DOMAIN" "$CERTBOT_VALIDATION"
+exec "${0%/*}/nsupdate.sh" -c /usr/local/etc/certbot/nsupdate.conf done "$CERTBOT_DOMAIN" "$CERTBOT_VALIDATION"
